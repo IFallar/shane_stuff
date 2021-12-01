6bd201588e3161ed7b9b2a54768cbbc8cf782adf
IFallar <75560427+IFallar@users.noreply.github.com>
2021-12-01 19:02:29 +0800

    Add files via upload

---
+++ js/mainD.js
@@ -0,0 +1,24 @@
+document.getElementById('erabutton').addEventListener('click', function() {
+    document.getElementById('cat_con_1').scrollIntoView({
+      behavior: 'smooth'
+    });
+});
+
+document.getElementById('locbutton').addEventListener('click', function() {
+    document.getElementById('cat_con_2').scrollIntoView({
+      behavior: 'smooth'
+    });
+});
+
+document.getElementById('evebutton').addEventListener('click', function() {
+    document.getElementById('cat_con_3').scrollIntoView({
+      behavior: 'smooth'
+    });
+});
+
+document.getElementById('themebutton').addEventListener('click', function() {
+    document.getElementById('cat_con_4').scrollIntoView({
+      behavior: 'smooth'
+    });
+});
+
