235f5da437dc956d1b48c9757d72663dc8bfafa7
Shane Ivan Fallar <75560427+IFallar@users.noreply.github.com>
2022-12-30 18:12:41 +0800

    Add files via upload

---
+++ countrycode.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class countrycode {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Enter a string: ");
+        Scanner scan = new Scanner(System.in);
+        String number = scan.nextLine();
+
+    }
+    
+}
